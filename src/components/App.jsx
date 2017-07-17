2d5a48c6dabf061af54a24321b384eb6ad4be1d7
Sean Prasertsit <sean.prasertsit@hulu.com>
2017-07-17 16:51:29 -0700

    :heavy_plus_sign: Added react and boilerplate

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className='perkgreed'>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
