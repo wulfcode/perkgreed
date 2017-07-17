8bdfc2b149e764ec53a289489bef882fe9c00bf6
Sean Prasertsit <sean@wulfcode.com>
2017-07-16 23:58:40 -0700

    :wrench: Added new configs and test static content

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body',
+});
+
 module.exports = {
   entry: './src/app.js',
   output: {
@@ -10,5 +18,8 @@ module.exports = {
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
-  }
+  },
+  plugins: [
+    HtmlWebpackPluginConfig,
+  ],
 }
