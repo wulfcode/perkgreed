4858eaa12f05764054626c9aa04bfbbc35c1fd26
Sean Prasertsit <sean@wulfcode.com>
2017-12-26 16:08:18 -0800

    :rotating_light: remove lint warnings

--- packages/perkgreed-mason/app.js
+++ packages/perkgreed-mason/app.js
@@ -1,8 +1,8 @@
-const koa = require('koa');
+const Koa = require('koa');
 const Router = require('koa-router');
 const routes = require('./routes');
 
-const app = new koa();
+const app = new Koa();
 const router = new Router();
 const PORT = 4001;
 const API_VERSION = 'v1';
@@ -11,6 +11,7 @@ router.use(`/${API_VERSION}`, routes.routes());
 app.use(router.routes());
 
 const server = app.listen(PORT, () => {
+  // eslint-disable-next-line
   console.log(`Server listening on port ${PORT}.`);
 });
 
